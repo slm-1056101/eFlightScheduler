53742bba11c480080fb55ea16c8e47e23b477729
Charles Muchene <charlesmuchene@gmail.com>
2019-10-15 15:03:00 -0500

    Implements default models

---
+++ src/main/java/edu/mum/cs/cs425/eFlightScheduler/models/Role.java
@@ -0,0 +1,10 @@
+package edu.mum.cs.cs425.eFlightScheduler.models;
+
+/**
+ * Role
+ * <p>
+ * The assigned role of the user
+ */
+public enum Role {
+    ADMIN, CONTROLLER
+}
---
+++ src/main/java/edu/mum/cs/cs425/eFlightScheduler/models/User.java
@@ -0,0 +1,65 @@
+package edu.mum.cs.cs425.eFlightScheduler.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * User class.
+ *
+ * Represents a user of the system e.g. admin etc
+ */
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String firstName, String lastName, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
