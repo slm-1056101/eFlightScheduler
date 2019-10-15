fd06b24372a0f9097f25e485145e3d1dc861753e
slm-1056101 <lsaidy9@gmail.com>
2019-10-15 14:11:06 -0500

    create spring project for automated flight scheduling

---
+++ src/main/java/edu/mum/cs/cs425/eFlightScheduler/EFlightSchedulerApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.eFlightScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFlightSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFlightSchedulerApplication.class, args);
+	}
+
+}
