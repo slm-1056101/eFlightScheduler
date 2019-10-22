40d47b05c7740d74e140f1895ce82e27f5e67fef
Charles Muchene <charlesmuchene@gmail.com>
2019-10-22 13:17:37 -0500

    Removes test data autorun

--- src/main/java/edu/mum/cs/cs425/eFlightScheduler/EFlightSchedulerApplication.java
+++ src/main/java/edu/mum/cs/cs425/eFlightScheduler/EFlightSchedulerApplication.java
@@ -1,6 +1,5 @@
 package edu.mum.cs.cs425.eFlightScheduler;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EFlightSchedulerApplication implements CommandLineRunner {
 
-    @Autowired
-    Test test;
-
     public static void main(String[] args) {
         SpringApplication.run(EFlightSchedulerApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        test.doMagic();
-	}
+    public void run(String... args) {
+        System.out.println("Running EFlight Scheduling System");
+    }
 }
