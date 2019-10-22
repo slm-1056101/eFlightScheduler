920112a5940f21b1c1907e9b505ca4274c7676e9
Kirubel <kirulucky12@gmail.com>
2019-10-22 10:08:01 -0500

    - finalizing the home page - ctreat the sechdule list page - adjusting the footer

--- src/main/java/edu/mum/cs/cs425/eFlightScheduler/controller/ScheduleViewController.java
+++ src/main/java/edu/mum/cs/cs425/eFlightScheduler/controller/ScheduleViewController.java
@@ -1,8 +1,12 @@
 package edu.mum.cs.cs425.eFlightScheduler.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.mum.cs.cs425.eFlightScheduler.service.IScheduleService;
 
 /**
  * Schedule controller
@@ -11,9 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/eflight/schedule")
 public class ScheduleViewController {
 
+    @Autowired
+    private IScheduleService service;
+
     @GetMapping("/")
-    public String home() {
-        return "public/schedule/index";
+    public ModelAndView showSechduled() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("sechduled", service.getAll());
+        mv.setViewName("public/schedule/index");
+        return mv;
+        //return "public/schedule/index";
     }
 
 }
